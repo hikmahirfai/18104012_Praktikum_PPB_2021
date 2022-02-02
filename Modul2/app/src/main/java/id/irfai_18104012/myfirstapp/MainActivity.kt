291351801cce45082327e90e133d7a9ac0388e25
hikmahirfai <hikmahirfai0411@gmail.com>
2022-02-02 11:59:23 +0700

    18104012_Praktikum_PPB_2021

---
+++ Modul2/app/src/main/java/id/irfai_18104012/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.irfai_18104012.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Modul6/Praktikum6/app/src/main/java/com/irfai/praktikum6/MyData.kt
@@ -0,0 +1,7 @@
+package com.irfai.praktikum6
+
+data class MyData (
+    var name : String,
+    var description : String,
+    var photo : String
+)
